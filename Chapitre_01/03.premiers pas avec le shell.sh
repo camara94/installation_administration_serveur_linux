5a69656d183a65fa38e99dccd8d9fd743c1d263d
Laby Damaro Camara <ldamaro98@gmail.com>
2021-03-30 21:12:38 +0200

    Chapitre 01

---
+++ Chapitre_01/03.premiers pas avec le shell.sh
@@ -0,0 +1,20 @@
+# affiche le shell courant
+echo $SHELL
+echo $0
+
+# montre le répertoire courant
+pwd
+
+# liste les fichiers
+ls
+
+# se déplace vers le répertoire parent
+cd ..
+# se déplace vers le répertoire racine
+cd /
+# se déplace vers le répertoire personnel
+cd ~
+cd
+
+# nettoie l'affichage
+clear
---
+++ Chapitre_01/04.premiers pas avec le shell2.sh
@@ -0,0 +1,14 @@
+# afficher la liste des fichiers et répertoires en format long
+ls -l
+ls --format=long
+
+# lister un sous-répertoire
+ls -l home
+
+# obtenir de l'aide
+ls --help
+man ls
+
+# afficher les fichier et répertoires cachés
+ls -a
+ls -al
